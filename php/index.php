48f194219b749ce0a99125c8a54c5ef1f7501707
David Morales <86642624+demoralesv@users.noreply.github.com>
2026-03-13 12:35:47 -0600

    Starting frontend

--- php/index.php
+++ php/index.php
@@ -8,5 +8,11 @@
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <link rel="stylesheet" href="/estilos/styles.css">
 <body>
+  <h1>Acortador de URLs</h1>
+  <form action="/acortar" method="post">
+    <label for="url">URL a acortar:</label>
+    <input type="text" id="url" name="url" required>
+    <button type="submit">Acortar</button>
+  </form>
 </body>
 </head>
