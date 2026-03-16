33a160f541bb56a2b306753a876d2ba030dd6db9
demoralesv <dmoralesv@estudiantec.cr>
2026-03-16 05:15:11 +0000

    COntrollers with logic done, still some trash to clean

--- php/redirectController.php
+++ php/redirectController.php
@@ -1,4 +1,9 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use ipinfo\ipinfo\IPinfo;
+
+$access_token = '0c678a2f046db3';
+$client = new IPinfo($access_token);
 $servername = "localhost";
 $username = "root";
 $password = "LmP_2k26";
@@ -12,12 +17,45 @@ $urlcode = basename($url);
 $sql = "select id, originalUrl from url where code = '$urlcode'";
 $result = $conn->query($sql);
 if($result->rowCount() > 0){
-$row = $result->fetch();
-header("Location: " . $row['originalUrl']);
+	$row = $result->fetch();
+	header("Location: " . $row['originalUrl']);
+	$urlid = $row['id'];
+	$infoC = $client->getDetails($ip);
+	$country = $infoC->country_name;
+	unset($result);
+	$sql = "select id from country where name = '$country'";
+	$result = $conn->query($sql);
+	if ($result->rowCount() > 0){
+		$row = $result->fetch();
+		$Cid = $row['id'];
+	}else{	
+		unset($result);
+		$sql = "INSERT INTO country (name) VALUES ('$country')";
+		$conn->exec($sql);
+		$Cid = $conn->lastInsertId();
+	}
+	unset($result);
+	$sql = "insert into countryxurl (idurl, idcountry, frequency) values ('$urlid','$Cid', 1) on duplicate key update frequency = frequency + 1";
+	$conn->exec($sql);
+	$sql = "select id from Dates where dates = curdate()";
+	$result = $conn->query($sql);
+	if ($result->rowCount() > 0) {
+		$row = $result->fetch();
+		$Did = $row['id'];
+	}else{
+		unset($result);
+		$sql = "insert into Dates (dates) values (curdate())";
+		$conn->exec($sql);
+		$Did = $conn->lastInsertId();
+	}
+	unset($result);
+	$sql = "insert into datexurl (idurl, iddate, frequency) values ('$urlid', '$Did', 1) on duplicate key update frequency = frequency + 1";
+	$conn->exec($sql);
+	$sql = "insert into accesslog (IP, idCountry, idDate, idUrl) values ('$ip','$Cid','$Did','$urlid')";
+	$conn->exec($sql);
 }else{
 header("Location: /index.php");
 }
 exit();
-
 
 ?>
